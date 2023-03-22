6e6a71ed9e4096ab320432932efd977121119032
Clumpmore <ivershinin21@gmail.com>
2023-03-22 21:01:24 +0300

    Task 1 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    You can return the answer in any order.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
-fun main(args: Array<String>) {
+fun main() {
     val nums1 = intArrayOf(2, 7, 11, 15)
     val nums2 = intArrayOf(3, 3, 4)
     val nums3 = intArrayOf(3, 3)
@@ -8,29 +8,32 @@ fun main(args: Array<String>) {
     val twoSum1 = twoSum(nums1, target1)
     val twoSum2 = twoSum(nums2, target2)
     val twoSum3 = twoSum(nums3, target3)
-
-
     println(twoSum1)
     println(twoSum2)
     println(twoSum3)
 }
 
 fun twoSum(nums: IntArray, target: Int): ArrayList<Int> {
-    var list = arrayListOf<Int>()
+    val list = arrayListOf<Int>()
     var checkSolution = false
-    for (i in 0 until nums.size-1) {
+    for (i in 0 until nums.size - 1) {
 
         for (j in 1 until nums.size) {
-            if (nums[i]+ nums[j]==target) {
-                list.add(i)
-                list.add(j)
-
+            if (i != j) {
+                if (nums[i] + nums[j] == target) {
+                    list.add(i)
+                    list.add(j)
+                    checkSolution = true
+                    break
+                }
             }
-            checkSolution = true
-            break
         }
         if (checkSolution) break
     }
+    if (list.size == 0) {
+        list.add(1)
+        list.add(2)
+    }
     return list
-
 }
+
