73a59da819b7f379bd95777ed3da1193e288daae
Clumpmore <ivershinin21@gmail.com>
2023-03-26 18:43:59 +0300

    14. Longest Common Prefix Write a function to find the longest common prefix string amongst an array of strings.
    If there is no common prefix, return an empty string "".

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,26 @@
 fun main() {
-    val romanMap: MutableMap<Char, Int> = HashMap()
-    romanMap['I']=1
-    romanMap['V']=5
-    romanMap['X']=10
-    romanMap['L']=50
-    romanMap['C']=100
-    romanMap['D']=500
-    romanMap['M']=1000
-    println(romanMap["XVI"[2]])
+
 }
 
-    class Solution {
-        fun romanToInt(s: String): Int {
-            val romanMap: MutableMap<Char, Int> = HashMap()
-            romanMap['I']=1
-            romanMap['V']=5
-            romanMap['X']=10
-            romanMap['L']=50
-            romanMap['C']=100
-            romanMap['D']=500
-            romanMap['M']=1000
-            val n = s.length
-            var num = romanMap[s[n-1]]!!
-            for (i in n-2 downTo 0) {
-                if (romanMap[s[i]]!! >= romanMap[s[i+1]]!!) {
-                    num += romanMap[s[i]]!!
-                } else  {
-                    num -= romanMap[s[i]]!!
+class Solution {
+    fun longestCommonPrefix(strs: Array<String>): String {
+        val longestCommonPrefix = StringBuilder()
+        if (strs.isEmpty()) {
+            return longestCommonPrefix.toString()
+        }
+        var minimumLength = strs[0].length
+        for (i in 1 until strs.size) {
+            minimumLength = minimumLength.coerceAtMost(strs[i].length)
+        }
+        for (i in 0 until minimumLength) {
+            val current = strs[0][i]
+            for (str in strs) {
+                if (str[i] != current) {
+                    return longestCommonPrefix.toString()
                 }
             }
-            return num
+            longestCommonPrefix.append(current)
         }
+        return longestCommonPrefix.toString()
     }
+}
