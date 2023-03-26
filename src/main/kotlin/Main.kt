e7f2add512a462ef4eebae3647619e1b71939d1d
Clumpmore <ivershinin21@gmail.com>
2023-03-26 19:02:15 +0300

    20. Valid Parentheses Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
    An input string is valid if:
    
    Open brackets must be closed by the same type of brackets.
    Open brackets must be closed in the correct order.
    Every close bracket has a corresponding open bracket of the same type.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,30 @@
+import java.util.*
+
 fun main() {
-    val nums1 = intArrayOf(2, 7, 11, 15)
-    val nums2 = intArrayOf(3, 3, 4)
-    val nums3 = intArrayOf(3, 3)
-    val target1 = 9
-    val target2 = 6
-    val target3 = 6
-    val twoSum1 = twoSum(nums1, target1)
-    val twoSum2 = twoSum(nums2, target2)
-    val twoSum3 = twoSum(nums3, target3)
-    println(twoSum1)
-    println(twoSum2)
-    println(twoSum3)
+
 }
 
-fun twoSum(nums: IntArray, target: Int): ArrayList<Int> {
-    val list = arrayListOf<Int>()
-    var checkSolution = false
-    for (i in 0 until nums.size - 1) {
-
-        for (j in 1 until nums.size) {
-            if (i != j) {
-                if (nums[i] + nums[j] == target) {
-                    list.add(i)
-                    list.add(j)
-                    checkSolution = true
-                    break
-                }
+class Solution {
+    fun isValid(s: String): Boolean {
+        val leftSymbols = Stack<Char>()
+        for (c in s.toCharArray()) {
+            if (c  == '(' || c == '{'||c =='[') {
+                leftSymbols.push(c)
+            }
+            else if (c == ')'&& !leftSymbols.isEmpty() && leftSymbols.peek() == '(') {
+                leftSymbols.pop()
+            } else if (c == '}'&& !leftSymbols.isEmpty() && leftSymbols.peek() == '{') {
+                leftSymbols.pop()
+            } else if (c == ']'&& !leftSymbols.isEmpty() && leftSymbols.peek() == '[') {
+                leftSymbols.pop()
+            }
+            else {
+                return false
             }
         }
-        if (checkSolution) break
+        return leftSymbols.isEmpty()
     }
-    if (list.size == 0) {
-        list.add(1)
-        list.add(2)
-    }
-    return list
 }
+
+
 
