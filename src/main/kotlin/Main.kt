3cbc05a56dfe9baf95d4d1f7af3c26e86669a4da
Clumpmore <ivershinin21@gmail.com>
2023-03-27 19:44:49 +0300

    27. Remove Element Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
    Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
    
    Return k after placing the final result in the first k slots of nums.
    
    Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,18 @@
 fun main() {
-    val nums1 = intArrayOf(2, 7, 11, 15)
-    val nums2 = intArrayOf(3, 3, 4)
-    val nums3 = intArrayOf(3, 3)
-    val target1 = 9
-    val target2 = 6
-    val target3 = 6
-    val twoSum1 = twoSum(nums1, target1)
-    val twoSum2 = twoSum(nums2, target2)
-    val twoSum3 = twoSum(nums3, target3)
-    println(twoSum1)
-    println(twoSum2)
-    println(twoSum3)
+val nums = intArrayOf(3,2,2,3)
+    val check = Solution().removeElement(nums,2)
+    println(check)
 }
+class Solution {
+    fun removeElement(nums: IntArray, `val`: Int): Int {
+var count = 0
+        for (i in nums.indices) {
+            if (nums[i]!=`val`) {
+                nums[count++]=nums[i]
 
-fun twoSum(nums: IntArray, target: Int): ArrayList<Int> {
-    val list = arrayListOf<Int>()
-    var checkSolution = false
-    for (i in 0 until nums.size - 1) {
-
-        for (j in 1 until nums.size) {
-            if (i != j) {
-                if (nums[i] + nums[j] == target) {
-                    list.add(i)
-                    list.add(j)
-                    checkSolution = true
-                    break
-                }
             }
-        }
-        if (checkSolution) break
-    }
-    if (list.size == 0) {
-        list.add(1)
-        list.add(2)
-    }
-    return list
-}
 
+        }
+        return count
+    }
+}
