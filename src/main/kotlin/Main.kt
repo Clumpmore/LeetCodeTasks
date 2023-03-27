9f8670242f5f4a278efcaa7131a0c4eddf7c0207
Clumpmore <ivershinin21@gmail.com>
2023-03-27 21:01:12 +0300

    28. Find the Index of the First Occurrence in a String
    Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,12 @@
 fun main() {
-    val nums1 = intArrayOf(2, 7, 11, 15)
-    val nums2 = intArrayOf(3, 3, 4)
-    val nums3 = intArrayOf(3, 3)
-    val target1 = 9
-    val target2 = 6
-    val target3 = 6
-    val twoSum1 = twoSum(nums1, target1)
-    val twoSum2 = twoSum(nums2, target2)
-    val twoSum3 = twoSum(nums3, target3)
-    println(twoSum1)
-    println(twoSum2)
-    println(twoSum3)
+    val haystack = "sadbadsad"
+    val needle = "sad"
+    val check = println(Solution().strStr(haystack, needle))
 }
 
-fun twoSum(nums: IntArray, target: Int): ArrayList<Int> {
-    val list = arrayListOf<Int>()
-    var checkSolution = false
-    for (i in 0 until nums.size - 1) {
-
-        for (j in 1 until nums.size) {
-            if (i != j) {
-                if (nums[i] + nums[j] == target) {
-                    list.add(i)
-                    list.add(j)
-                    checkSolution = true
-                    break
-                }
-            }
-        }
-        if (checkSolution) break
+class Solution {
+    fun strStr(haystack: String, needle: String): Int {
+        return if (needle==null) 0
+        else haystack.indexOf(needle)
     }
-    if (list.size == 0) {
-        list.add(1)
-        list.add(2)
-    }
-    return list
 }
-
