9e3f941286518edf99d721daa04d11a2eec1b2f2
Clumpmore <ivershinin21@gmail.com>
2023-03-28 19:36:12 +0300

    35. Search Insert Position
    Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
    
    You must write an algorithm with O(log n) runtime complexity.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,19 @@
 fun main() {
-    val nums1 = intArrayOf(2, 7, 11, 15)
-    val nums2 = intArrayOf(3, 3, 4)
-    val nums3 = intArrayOf(3, 3)
-    val target1 = 9
-    val target2 = 6
-    val target3 = 6
-    val twoSum1 = twoSum(nums1, target1)
-    val twoSum2 = twoSum(nums2, target2)
-    val twoSum3 = twoSum(nums3, target3)
-    println(twoSum1)
-    println(twoSum2)
-    println(twoSum3)
+
 }
 
-fun twoSum(nums: IntArray, target: Int): ArrayList<Int> {
-    val list = arrayListOf<Int>()
-    var checkSolution = false
-    for (i in 0 until nums.size - 1) {
-
-        for (j in 1 until nums.size) {
-            if (i != j) {
-                if (nums[i] + nums[j] == target) {
-                    list.add(i)
-                    list.add(j)
-                    checkSolution = true
-                    break
-                }
-            }
+class Solution {
+    fun searchInsert(nums: IntArray, target: Int): Int {
+        var start = 0
+        var end = nums.size - 1
+        while (start <= end) {
+            var mid = (start + end) / 2
+            if (target > nums[mid]) start = mid + 1
+            else if (target < nums[mid]) end = mid - 1
+            else return mid
         }
-        if (checkSolution) break
+        return start
     }
-    if (list.size == 0) {
-        list.add(1)
-        list.add(2)
-    }
-    return list
 }
+
 
