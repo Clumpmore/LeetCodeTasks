2e95f2f8db17da32901e7b512ef4787edfaea789
Clumpmore <ivershinin21@gmail.com>
2023-03-28 20:48:49 +0300

    58. Length of Last Word
    Given a string s consisting of words and spaces, return the length of the last word in the string.
    
    A word is a maximal
    substring
     consisting of non-space characters only.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,20 @@
 fun main() {
-    val nums1 = intArrayOf(2, 7, 11, 15)
-    val nums2 = intArrayOf(3, 3, 4)
-    val nums3 = intArrayOf(3, 3)
-    val target1 = 9
-    val target2 = 6
-    val target3 = 6
-    val twoSum1 = twoSum(nums1, target1)
-    val twoSum2 = twoSum(nums2, target2)
-    val twoSum3 = twoSum(nums3, target3)
-    println(twoSum1)
-    println(twoSum2)
-    println(twoSum3)
+    val s ="a "
+    println(s.length-1)
+println(Solution().lengthOfLastWord(s))
 }
 
-fun twoSum(nums: IntArray, target: Int): ArrayList<Int> {
-    val list = arrayListOf<Int>()
-    var checkSolution = false
-    for (i in 0 until nums.size - 1) {
-
-        for (j in 1 until nums.size) {
-            if (i != j) {
-                if (nums[i] + nums[j] == target) {
-                    list.add(i)
-                    list.add(j)
-                    checkSolution = true
-                    break
-                }
+class Solution {
+    fun lengthOfLastWord(s: String): Int {
+       var count = 0
+        var copy = s.trim()
+        var start = copy.length-1
+        for (i in start.downTo(0)) {
+            if (copy[i]==' ') {
+                break
             }
+            count++
         }
-        if (checkSolution) break
+        return count
     }
-    if (list.size == 0) {
-        list.add(1)
-        list.add(2)
-    }
-    return list
 }
-
