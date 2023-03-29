6552d1ad2bf8237bfb2e358367d31fabce401d0e
Clumpmore <ivershinin21@gmail.com>
2023-03-29 20:49:35 +0300

    83. Remove Duplicates from Sorted List Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,21 @@
 fun main() {
-    val nums1 = intArrayOf(2, 7, 11, 15)
-    val nums2 = intArrayOf(3, 3, 4)
-    val nums3 = intArrayOf(3, 3)
-    val target1 = 9
-    val target2 = 6
-    val target3 = 6
-    val twoSum1 = twoSum(nums1, target1)
-    val twoSum2 = twoSum(nums2, target2)
-    val twoSum3 = twoSum(nums3, target3)
-    println(twoSum1)
-    println(twoSum2)
-    println(twoSum3)
-}
 
-fun twoSum(nums: IntArray, target: Int): ArrayList<Int> {
-    val list = arrayListOf<Int>()
-    var checkSolution = false
-    for (i in 0 until nums.size - 1) {
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
 
-        for (j in 1 until nums.size) {
-            if (i != j) {
-                if (nums[i] + nums[j] == target) {
-                    list.add(i)
-                    list.add(j)
-                    checkSolution = true
-                    break
-                }
+    fun deleteDuplicates(head: ListNode?): ListNode? {
+        if (head?.next == null) return head
+        var h1 = head
+        while (h1!!.next!=null) {
+            if (h1.`val`==h1.next!!.`val`) {
+                h1.next = h1.next!!.next
+            } else {
+                h1 = h1.next
             }
         }
-        if (checkSolution) break
+        return head
     }
-    if (list.size == 0) {
-        list.add(1)
-        list.add(2)
-    }
-    return list
 }
+
 
