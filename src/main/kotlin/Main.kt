304d479580d9aa08ae6b53c93ff85a5d12b811aa
Clumpmore <ivershinin21@gmail.com>
2023-03-29 19:07:04 +0300

    67. Add Binary Given two binary strings a and b, return their sum as a binary string.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,29 +1,35 @@
 fun main() {
-
+    var a = "100"
+    var b = "101"
+    var check = Solution().addBinary(a, b)
+    println(check)
+    println(a[2] - '0')
+    println(b[2] - '0')
 }
 
 class Solution {
-    fun plusOne(digits: IntArray): IntArray {
-
-        var i = digits.size - 1;
-
-        while (i >= 0 && digits[i] == 9) {
-            i--
+    fun addBinary(a: String, b: String): String = StringBuilder().apply {
+        var o = 0
+        var i = a.lastIndex
+        var j = b.lastIndex
+        while (i >= 0 || j >= 0 || o == 1) {
+            var num = o
+            o = 0
+            if (i >= 0 && a[i--] == '1') num++
+            if (j >= 0 && b[j--] == '1') num++
+            when (num) {
+                0 -> append('0')
+                1 -> append('1')
+                2 -> {
+                    append('0')
+                    o = 1
+                }
+                else -> {
+                    append('1')
+                    o = 1
+                }
+            }
         }
-
-
-        if (i == -1) {
-            var result = IntArray(digits.size + 1)
-            result[0] = 1;
-            return result;
-        }
-        var result = IntArray(digits.size)
-
-        result[i] = digits[i] + 1;
-        for (j in 0 until i) {
-            result[j] = digits[j]
-        }
-
-        return result;
-    }
+    }.reverse().toString()
+}
 }
