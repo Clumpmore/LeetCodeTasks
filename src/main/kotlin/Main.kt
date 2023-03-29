a25c39fe7e356bd0e64e7d8b6a5c49d11f4a38d9
Clumpmore <ivershinin21@gmail.com>
2023-03-29 19:27:17 +0300

    69. Sqrt(x)
    Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
    
    You must not use any built-in exponent function or operator.
    
    For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,17 @@
 fun main() {
-    val nums1 = intArrayOf(2, 7, 11, 15)
-    val nums2 = intArrayOf(3, 3, 4)
-    val nums3 = intArrayOf(3, 3)
-    val target1 = 9
-    val target2 = 6
-    val target3 = 6
-    val twoSum1 = twoSum(nums1, target1)
-    val twoSum2 = twoSum(nums2, target2)
-    val twoSum3 = twoSum(nums3, target3)
-    println(twoSum1)
-    println(twoSum2)
-    println(twoSum3)
+    println(mySqrt(5))
 }
 
-fun twoSum(nums: IntArray, target: Int): ArrayList<Int> {
-    val list = arrayListOf<Int>()
-    var checkSolution = false
-    for (i in 0 until nums.size - 1) {
-
-        for (j in 1 until nums.size) {
-            if (i != j) {
-                if (nums[i] + nums[j] == target) {
-                    list.add(i)
-                    list.add(j)
-                    checkSolution = true
-                    break
-                }
-            }
-        }
-        if (checkSolution) break
+fun mySqrt(x: Int): Int {
+    if (x<2) return x
+    var left = 2
+    var right = x/2
+    while (left<=right) {
+        var midd = left+(right-left)/2
+        var squeareMidd:Long = midd.toLong()*midd.toLong()
+        if (squeareMidd>x) right = midd-1
+        else if (squeareMidd<x) left = midd+1
+        else return midd
     }
-    if (list.size == 0) {
-        list.add(1)
-        list.add(2)
-    }
-    return list
+return right
 }
-
