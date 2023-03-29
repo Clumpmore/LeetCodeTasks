3c758c5a40976da160698c9dd60ca1810f86603d
Clumpmore <ivershinin21@gmail.com>
2023-03-29 20:07:29 +0300

    70. Climbing Stairs
    You are climbing a staircase. It takes n steps to reach the top.
    
    Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,14 @@
 fun main() {
-    val nums1 = intArrayOf(2, 7, 11, 15)
-    val nums2 = intArrayOf(3, 3, 4)
-    val nums3 = intArrayOf(3, 3)
-    val target1 = 9
-    val target2 = 6
-    val target3 = 6
-    val twoSum1 = twoSum(nums1, target1)
-    val twoSum2 = twoSum(nums2, target2)
-    val twoSum3 = twoSum(nums3, target3)
-    println(twoSum1)
-    println(twoSum2)
-    println(twoSum3)
+
 }
 
-fun twoSum(nums: IntArray, target: Int): ArrayList<Int> {
-    val list = arrayListOf<Int>()
-    var checkSolution = false
-    for (i in 0 until nums.size - 1) {
-
-        for (j in 1 until nums.size) {
-            if (i != j) {
-                if (nums[i] + nums[j] == target) {
-                    list.add(i)
-                    list.add(j)
-                    checkSolution = true
-                    break
-                }
-            }
-        }
-        if (checkSolution) break
+fun climbStairs(n: Int): Int {
+if(n<=3) return n
+    var a =3
+    var b=2
+    for (i in 0 until n-3) {
+        a+=b
+        b= a-b
     }
-    if (list.size == 0) {
-        list.add(1)
-        list.add(2)
-    }
-    return list
+    return a
 }
-
