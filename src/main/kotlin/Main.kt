5523a3f976b2248be99f4d0187c2443ad0aaabb5
Clumpmore <ivershinin21@gmail.com>
2023-03-30 20:51:16 +0300

    100. Same Tree Given the roots of two binary trees p and q, write a function to check if they are the same or not.
    Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,20 @@
+import java.util.*
+
 fun main() {
-    val nums1 = intArrayOf(2, 7, 11, 15)
-    val nums2 = intArrayOf(3, 3, 4)
-    val nums3 = intArrayOf(3, 3)
-    val target1 = 9
-    val target2 = 6
-    val target3 = 6
-    val twoSum1 = twoSum(nums1, target1)
-    val twoSum2 = twoSum(nums2, target2)
-    val twoSum3 = twoSum(nums3, target3)
-    println(twoSum1)
-    println(twoSum2)
-    println(twoSum3)
+
 }
 
-fun twoSum(nums: IntArray, target: Int): ArrayList<Int> {
-    val list = arrayListOf<Int>()
-    var checkSolution = false
-    for (i in 0 until nums.size - 1) {
-
-        for (j in 1 until nums.size) {
-            if (i != j) {
-                if (nums[i] + nums[j] == target) {
-                    list.add(i)
-                    list.add(j)
-                    checkSolution = true
-                    break
-                }
-            }
-        }
-        if (checkSolution) break
+fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+    if (p==null && q==null) return true
+    var node1 = p
+    var node2 = q
+    if (node1!=null && node2!=null ) {
+        return  (p!!.`val`==q!!.`val` && isSameTree(p.left,q.left) && isSameTree(p.right,q.right))
     }
-    if (list.size == 0) {
-        list.add(1)
-        list.add(2)
-    }
-    return list
+    return false
 }
 
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
