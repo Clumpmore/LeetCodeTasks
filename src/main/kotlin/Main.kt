e4e24adcaa71706e71a2d1bcf55cb22c5309b05b
Clumpmore <ivershinin21@gmail.com>
2023-03-30 20:33:48 +0300

    94. Binary Tree Inorder Traversal
    Given the root of a binary tree, return the inorder traversal of its nodes' values.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,29 @@
+import com.sun.source.tree.Tree
+import java.util.ArrayList
+import java.util.Stack
+
 fun main() {
-    val nums1 = intArrayOf(2, 7, 11, 15)
-    val nums2 = intArrayOf(3, 3, 4)
-    val nums3 = intArrayOf(3, 3)
-    val target1 = 9
-    val target2 = 6
-    val target3 = 6
-    val twoSum1 = twoSum(nums1, target1)
-    val twoSum2 = twoSum(nums2, target2)
-    val twoSum3 = twoSum(nums3, target3)
-    println(twoSum1)
-    println(twoSum2)
-    println(twoSum3)
+
 }
 
-fun twoSum(nums: IntArray, target: Int): ArrayList<Int> {
-    val list = arrayListOf<Int>()
-    var checkSolution = false
-    for (i in 0 until nums.size - 1) {
+fun inorderTraversal(root: TreeNode?): List<Int> {
+    var list = ArrayList<Int>()
+    var stack = Stack<TreeNode>()
+    if (root == null) return list
+    var currentNode = root
 
-        for (j in 1 until nums.size) {
-            if (i != j) {
-                if (nums[i] + nums[j] == target) {
-                    list.add(i)
-                    list.add(j)
-                    checkSolution = true
-                    break
-                }
-            }
+    while (currentNode != null || !stack.isEmpty()) {
+        while (currentNode != null) {
+            stack.push(currentNode)
+            currentNode = currentNode!!.left
         }
-        if (checkSolution) break
-    }
-    if (list.size == 0) {
-        list.add(1)
-        list.add(2)
+        list.add(stack.peek().`val`)
+        currentNode = stack.pop().right
     }
     return list
 }
 
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
